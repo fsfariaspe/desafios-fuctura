facb40251eb70f116319f8ccef9fc84fee103473
Flávio Farias <fsfariaspe@gmail.com>
2021-05-08 19:07:30 -0300

    Exercícios da aula de 050521 de while e for

---
+++ ExercicioFor10x.java
@@ -0,0 +1,15 @@
+package Exercicio2;
+
+public class ExercicioFor10x {
+
+	public static void main(String[] args) {
+		
+		String nome = "Flávio Farias vai ser programador back-end.";
+		
+		for(int i =1; i <= 10; i++) {
+			
+			System.out.println(i + " : " + nome);
+		}
+	}
+
+}
---
+++ ExercicioWhile10x.java
@@ -0,0 +1,18 @@
+package Exercicio1;
+
+public class ExercicioWhile10x {
+
+	public static void main(String[] args) {
+		
+		String nome = "Flávio Farias vai ser programador back-end.";
+		int i = 1;
+		
+		while(i <= 10) {
+			
+			System.out.println(i + " : " + nome);
+			i++;
+		}
+
+	}
+
+}
